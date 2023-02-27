9c9f968583b92c7317111aaa222814bb24702eb2
rajeevan <naveejr@gmail.com>
2023-02-28 01:17:19 +0530

    Graph QL Queries

--- Server/src/main/java/com/naveejr/server/ServerApplication.java
+++ Server/src/main/java/com/naveejr/server/ServerApplication.java
@@ -1,7 +1,16 @@
 package com.naveejr.server;
 
+import com.naveejr.server.bo.Author;
+import com.naveejr.server.bo.Book;
+import com.naveejr.server.repository.AuthorRepository;
+import com.naveejr.server.repository.BookRepository;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -9,5 +18,20 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+
+	@Bean
+	ApplicationRunner applicationRunner(AuthorRepository authorRepository, BookRepository bookRepository) {
+		return args -> {
+			Author josh = authorRepository.save(new Author(null, "Josh Long"));
+			Author mark = authorRepository.save(new Author(null, "Mark Heckler"));
+			bookRepository.saveAll(List.of(
+					new Book("Reactive Spring", "Manning", josh),
+					new Book("Cloud Native Jave", "Manning", josh),
+					new Book("Springboot", "Manning", mark)
+
+			));
+		};
+	}
+
 
 }
---
+++ Server/src/main/java/com/naveejr/server/controller/AuthorController.java
@@ -0,0 +1,30 @@
+package com.naveejr.server.controller;
+
+import com.naveejr.server.bo.Author;
+import com.naveejr.server.repository.AuthorRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
+public class AuthorController {
+
+	private final AuthorRepository authorRepository;
+
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
+	}
+
+	@QueryMapping
+	Iterable<Author> authors() {
+		return authorRepository.findAll();
+	}
+
+	@QueryMapping
+	Optional<Author> authorById(@Argument Long id) {
+		return authorRepository.findById(id);
+	}
+
+}
