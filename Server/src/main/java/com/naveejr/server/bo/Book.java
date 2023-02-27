a13e25544c45be5e860d1e36cebd319d4d7501c5
rajeevan <naveejr@gmail.com>
2023-02-28 01:26:42 +0530

    Graph QL Mutation

--- Server/src/main/java/com/naveejr/server/bo/Book.java
+++ Server/src/main/java/com/naveejr/server/bo/Book.java
@@ -55,4 +55,7 @@ public class Book {
 
 	public Book() {
 	}
+
 }
+
+
--- Server/src/main/java/com/naveejr/server/controller/AuthorController.java
+++ Server/src/main/java/com/naveejr/server/controller/AuthorController.java
@@ -1,8 +1,11 @@
 package com.naveejr.server.controller;
 
 import com.naveejr.server.bo.Author;
+import com.naveejr.server.bo.Book;
 import com.naveejr.server.repository.AuthorRepository;
+import com.naveejr.server.repository.BookRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -13,8 +16,11 @@ public class AuthorController {
 
 	private final AuthorRepository authorRepository;
 
-	public AuthorController(AuthorRepository authorRepository) {
+	private final BookRepository bookRepository;
+
+	public AuthorController(AuthorRepository authorRepository, BookRepository bookRepository) {
 		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
 	}
 
 	@QueryMapping
@@ -25,6 +31,18 @@ public class AuthorController {
 	@QueryMapping
 	Optional<Author> authorById(@Argument Long id) {
 		return authorRepository.findById(id);
+	}
+
+	@MutationMapping
+	Book addBook(@Argument BookInput book) {
+		Author author = authorRepository.findById(book.authorId).orElseThrow(() -> new IllegalArgumentException("Author not found"));
+
+		Book b = new Book(book.title, book.publisher, author);
+		return bookRepository.save(b);
+	}
+
+
+	record BookInput(String title, String publisher, Long authorId) {
 	}
 
 }
